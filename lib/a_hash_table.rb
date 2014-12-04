1e40228987cb0c4b75adee97a8c2de850371b876
Steve Annessa <steve.annessa@ziplist.com>
2014-12-04 18:09:18 -0500

    Expand the hash table to be a chained hash table using linked lists
    - Doesn't take care of collisions with the exact same key
    - Probably not optimized in cases of removing and finding
    - Does allow the same hash bucket to be used multiple times but with worse
      performance as times goes on

--- lib/a_hash_table.rb
+++ lib/a_hash_table.rb
@@ -1,3 +1,6 @@
+$:.unshift File.dirname(__FILE__)
+require 'a_linked_list'
+
 class AHashTable
 
   # arbitrary limits used for test cases
@@ -12,16 +15,50 @@ class AHashTable
   end
 
   def insert(hsh_key, hsh_val)
-    @hsh_arry[compute_hash(hsh_key)] = hsh_val
+    index = compute_hash(hsh_key)
+
+    # first check to see something exists at the index
+    if @hsh_arry[index] == nil
+
+      # there is nothing here, create a new linkedlist with a single node
+      # containg an array of [key, val]
+      a_ll = ALinkedList.new
+      a_ll.add([hsh_key, hsh_val])
+      @hsh_arry[index] = a_ll
+    else
+        # there is something here so we must add it to the linkedlist
+        # with the value of the linkedlist node being an array containing
+        # the key and the value
+        a_ll = @hsh_arry[index]
+        a_ll.add([hsh_key, hsh_val])
+    end
     hsh_val
   end
 
   def find(hsh_key)
-    @hsh_arry[compute_hash(hsh_key)]
+    index = compute_hash(hsh_key)
+    return_val = nil
+    a_ll = @hsh_arry[index]
+    a_ll.each do |n|
+      if n.val.first == hsh_key
+        return_val = n.val.last
+        break
+      end
+    end
+    return_val
   end
 
   def remove(hsh_key)
-    @hsh_arry[compute_hash(hsh_key)] = nil
+    index = compute_hash(hsh_key)
+
+    if empty?(index)
+      raise "Nothing to remove"
+    end
+
+    a_ll = @hsh_arry[index]
+    hsh_val = find(hsh_key)
+    a_ll.remove([hsh_key, hsh_val])
+    hsh_val
   end
 
   def valid_size?(table_size)
@@ -30,6 +67,15 @@ class AHashTable
             " #{MAXIMUM_TABLE_SIZE}."
     end
     return true
+  end
+
+  def empty?(index)
+    # determine if the index is empty
+    if (@hsh_arry[index] == nil) || (@hsh_arry[index].size == 0)
+      return true
+    else
+      return false
+    end
   end
 
   def compute_hash(hsh_key)
