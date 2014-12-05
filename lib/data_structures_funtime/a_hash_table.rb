c3e35669cadcf112fac5f3425bf1c8dc99714e37
Steve Annessa <steve.annessa@ziplist.com>
2014-12-04 23:43:48 -0500

    Add documentation for AHashTable

--- lib/data_structures_funtime/a_hash_table.rb
+++ lib/data_structures_funtime/a_hash_table.rb
@@ -2,10 +2,13 @@ module DataStructuresFuntime
   require_relative 'a_linked_list'
 
   class AHashTable
-    # arbitrary limits used for test cases
     MINIMUM_TABLE_SIZE = 1
     MAXIMUM_TABLE_SIZE = 10
 
+    # Creates an instance of AHashTable
+    #
+    # @param table_size [Fixnum] the size of the table to be created
+    # @return [AHashTable] the instantiated AHashTable Object
     def initialize(table_size)
       # run a sanity check to make sure table_size is valid
       if valid_size?(table_size)
@@ -13,6 +16,11 @@ module DataStructuresFuntime
       end
     end
 
+    # Inserts a key/value pair into the hash table
+    #
+    # @param hsh_key [Fixnum] the hash key
+    # @param hsh_val [Object] the hash value
+    # @return [Object] the value inserted into the hash
     def insert(hsh_key, hsh_val)
       index = compute_hash(hsh_key)
 
@@ -33,6 +41,11 @@ module DataStructuresFuntime
       hsh_val
     end
 
+    # Returns the value of the key, if it exists, nil otherwise
+    #
+    # @param hsh_key [Fixnum] the hash key
+    # @return [Object] the value of supplied key
+    # @return nil if no key found
     def find(hsh_key)
       index = compute_hash(hsh_key)
       return_val = nil
@@ -50,6 +63,11 @@ module DataStructuresFuntime
       return_val
     end
 
+    # Removes a key entry from the hash table
+    #
+    # @param hsh_key [Fixnum] the hash key to be removed
+    # @return [Object] the value of the removed key
+    # @raise [RuntimeError] if the key is not found
     def remove(hsh_key)
       index = compute_hash(hsh_key)
 
@@ -63,14 +81,12 @@ module DataStructuresFuntime
       hsh_val
     end
 
-    def valid_size?(table_size)
-      if (table_size < MINIMUM_TABLE_SIZE) || (table_size > MAXIMUM_TABLE_SIZE)
-        raise "Table size must be more than #{MINIMUM_TABLE_SIZE} and less than" +
-              " #{MAXIMUM_TABLE_SIZE}."
-      end
-      return true
-    end
+    private
 
+    # Determine if the table index contains a valid entry
+    #
+    # @param index [Fixnum] the index of the hash table array
+    # @return [Boolean] whether or not the index exists
     def empty?(index)
       # determine if the index is empty
       if (@hsh_arry[index] == nil) || (@hsh_arry[index].size == 0)
@@ -80,6 +96,23 @@ module DataStructuresFuntime
       end
     end
 
+    # Determine if the supplied size is within table contraints
+    #
+    # @param table_size [Fixnum] the size of the table to be created
+    # @return [Boolean] the table size is sufficient
+    # @raise [RuntimeError] the table size is out of bounds
+    def valid_size?(table_size)
+      if (table_size < MINIMUM_TABLE_SIZE) || (table_size > MAXIMUM_TABLE_SIZE)
+        raise "Table size must be more than #{MINIMUM_TABLE_SIZE} and less than" +
+              " #{MAXIMUM_TABLE_SIZE}."
+      end
+      return true
+    end
+
+    # Takes a hash key and creates an index to be used in the array
+    #
+    # @param hsh_key [Fixnum] the hash key
+    # @return [Fixnum] the index to be used for the key
     def compute_hash(hsh_key)
       hsh_key % @hsh_arry.size
     end
