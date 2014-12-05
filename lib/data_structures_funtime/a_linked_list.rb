119c26b37f884d76483695ae5b6b6950e1cdccc7
Steve Annessa <steve.annessa@ziplist.com>
2014-12-05 00:00:08 -0500

    Add documentation for ALinkedList

--- lib/data_structures_funtime/a_linked_list.rb
+++ lib/data_structures_funtime/a_linked_list.rb
@@ -4,26 +4,38 @@ module DataStructuresFuntime
   class ALinkedList
     HEAD_VAL = "**HEAD**"
 
-    # init
+    # Creates an instance of ALinkedList
+    #
+    # @return [ALinkedList] the instantiated ALinkedList Object
     def initialize
       @head = ANode.new(HEAD_VAL)
       @curr = @head
       @size = 0
     end
 
+    # Returns the head of the linked list
+    #
+    # @return [ANode] the head of the linked list
     def head
       @head
     end
 
-    # returns the current node
+    # Returns the current pointer of the linked list
+    #
+    # @return [ANode] the current pointer of the linked list
     def curr
       @curr
     end
 
+    # Returns the size of the linked list
     def size
       @size
     end
 
+    # Add an Object to the linked list
+    #
+    # @param val [Object] the object to be added to the linked list
+    # @return [ANode] the new ANode created
     def add(val)
       new_node = ANode.new(val)
       @curr.set_next(new_node)
@@ -31,6 +43,12 @@ module DataStructuresFuntime
       @curr = new_node
     end
 
+    # Remove an Object from the linked list
+    #
+    # May cause an O(n) operation
+    #
+    # @param val [Object] the object to be removed from the linked list
+    # @return [Object] the removed Object
     def remove(val)
       # iterate through linkedlist until you find the val, then remove it
       curr = @head
@@ -46,6 +64,9 @@ module DataStructuresFuntime
       val
     end
 
+    # An iterator that yields each element to the caller
+    #
+    # @return [ANode] the entire linked list
     def each
       if @size > 0
 
