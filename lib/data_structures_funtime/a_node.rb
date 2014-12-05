c3557593760a183ef5c434044d64dfa96242cca7
Steve Annessa <steve.annessa@ziplist.com>
2014-12-04 23:13:17 -0500

    Create basic documentation for ANode

--- lib/data_structures_funtime/a_node.rb
+++ lib/data_structures_funtime/a_node.rb
@@ -5,30 +5,55 @@ module DataStructuresFuntime
     @next = nil
     @prev = nil
 
+    # Creates a new ANode instance
+    #
+    # @param val [Object] the value to be stored
+    # @return [ANode] the instantiated ANode Object
     def initialize(val)
       @val = val
     end
 
+    # Sets the next field of the node to the specificied ANode instance
+    #
+    # @param next_node [ANode] the node to be connected
+    # @return [ANode] the next ANode Object
     def set_next(next_node)
       @next = next_node
     end
 
+    # Returns the next ANode Object
+    #
+    # @return [ANode] the next ANode Object
     def next
       @next
     end
 
+    # Sets the prev field of the node to the specificied ANode instance
+    #
+    # @param prev_node [ANode] the node to be connected
+    # @return [ANode] the previous ANode Object
     def set_prev(prev_node)
       @prev = prev_node
     end
 
+    # Returns the previous ANode instance
+    #
+    # @return [ANode] the prev ANode Object
     def prev
       @prev
     end
 
+    # Sets the val field of the node to the specificied Object
+    #
+    # @param val [Object] the value for the node
+    # @return [Object] the value for the node
     def set_val(val)
       @val = val
     end
 
+    # Returns the value of the ANode instance
+    #
+    # @return [Object] the value of the ANode instance
     def val
       @val
     end
