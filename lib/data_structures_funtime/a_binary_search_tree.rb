dec82dc771acbf51a75446fdee2c0fe51a01b3d3
Steve Annessa <steve.annessa@ziplist.com>
2014-12-06 17:06:26 -0500

    Make insert and a few traversal methods for the BST - WIP

--- lib/data_structures_funtime/a_binary_search_tree.rb
+++ lib/data_structures_funtime/a_binary_search_tree.rb
@@ -1,26 +1,19 @@
 module DataStructuresFuntime
   require_relative "a_node"
+  require_relative "a_linked_list"
 
   class ABinarySearchTree
-    PARENT_VALUE = "**PARENT**"
-
-    # Create an instance of a binary search tree
-    def initialize
-      # create the first node, we'll call parent
-      @parent = ANode.new(PARENT_VALUE)
-    end
-
     # The function that compares nodes of the tree
     #
-    # @param l_node [ANode] the left value to be compared
-    # @param r_node [ANode] the right value to be compared
+    # @param key [Fixnum] the left value to be compared
+    # @param node [ANode] the right value to be compared
     # @return [Fixnum] -1 for less than, 0 for equal, 1 for greater than
-    def comparator(l_node, r_node)
+    def comparator(key, r_node)
       return_val = nil
 
-      if l_node.val < r_node.val
+      if key < r_node.val
         return_val = -1
-      elsif l_node.val > r_node.val
+      elsif key > r_node.val
         return_val = 1
       else
         return_val = 0
@@ -29,5 +22,116 @@ module DataStructuresFuntime
       return_val
     end
 
+    # Add a value to the tree
+    #
+    # @param val [Fixnum] the value to be added to the tree
+    def insert(val)
+      if @parent.nil?
+        return @parent = ANode.new(val)
+      end
+
+      # find where the val belongs
+      new_node = ANode.new(val)
+      recurse_insert(@parent, new_node)
+    end
+
+    def recurse_insert(parent, new_node)
+      if parent.nil?
+        return new_node
+      elsif new_node.val < parent.val
+        # insert to the left
+	if parent.left.nil?
+	  return parent.set_left(new_node)
+	else
+	  recurse_insert(parent.left, new_node)
+	end
+      elsif new_node.val > parent.val
+        # insert to the right
+	if parent.right.nil?
+	  return parent.set_right(new_node)
+	else
+	  recurse_insert(parent.right, new_node)
+	end
+      end
+    end
+
+    # Find a node of specific value in the tree
+    #
+    # @param parent [ANode] the current parent of the tree
+    # @param val [Fixnum] the value to be positioned in the tree
+    def find(parent, val)
+      if parent.nil? || (comparator(val, parent) == 0)
+        return parent
+      elsif comparator(val, parent) == -1
+        # node is less than parent
+	find(parent.left, val)
+      elsif comparator(val, parent) == 1
+        # node is more than parent
+	find(parent.right, val)
+      end
+    end
+
+    def search(val)
+      return nil if @parent.nil?
+
+      find(@parent, val)
+    end
+
+    def clear
+      @parent = nil
+    end
+
+    def print_out_tree
+      node = @parent
+      while(!node.nil?)
+        puts node.val
+
+	if !(node.left.nil?)
+	  puts '/'
+	elsif !(node.right.nil?)
+	  puts "\\"
+	end
+	node = node.left
+      end
+    end
+
+    def traverse_tree(parent)
+      return if parent.nil?
+      puts parent.val
+      if !parent.left.nil?
+        puts "left of #{parent.val} is: "
+        traverse_tree(parent.left)
+      end
+      if !parent.right.nil?
+        puts "right of #{parent.val} is: "
+        traverse_tree(parent.right)
+      end
+      if parent.left.nil? && parent.right.nil?
+        return
+      end
+    end
+
+    def root_node
+      @parent
+    end
+
+    def print_out_specific_level(level)
+      traverse_specific_level(root_node, 1, level)
+    end
+
+    def traverse_specific_level(node, current_level, desired_level)
+      if (current_level == desired_level)
+        puts node.val
+	return
+      end
+
+      if !node.left.nil?
+	traverse_specific_level(node.left, current_level + 1, desired_level)
+      end
+
+      if !node.right.nil?
+        traverse_specific_level(node.right, current_level + 1, desired_level)
+      end
+    end
   end
 end
--- test/a_binary_search_tree_test.rb
+++ test/a_binary_search_tree_test.rb
@@ -14,19 +14,19 @@ class ABinarySearchTreeTest < Test::Unit::TestCase
     # test left node less than right node
     l_node = DataStructuresFuntime::ANode.new(1)
     r_node = DataStructuresFuntime::ANode.new(3)
-    res = abst.comparator(l_node, r_node)
+    res = abst.comparator(l_node.val, r_node)
     assert_equal -1, res
 
     # test left node greater than right node
     l_node = DataStructuresFuntime::ANode.new(8)
     r_node = DataStructuresFuntime::ANode.new(6)
-    res = abst.comparator(l_node, r_node)
+    res = abst.comparator(l_node.val, r_node)
     assert_equal 1, res
 
     # test left node equal to right node
     l_node = DataStructuresFuntime::ANode.new(7)
     r_node = DataStructuresFuntime::ANode.new(7)
-    res = abst.comparator(l_node, r_node)
+    res = abst.comparator(l_node.val, r_node)
     assert_equal 0, res
   end
 end
